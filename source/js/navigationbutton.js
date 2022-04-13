251b1a4537fc8f36062dabfd21039c7a8dc6acb8
Regeace <playmarket_pochta@mail.ru>
2022-04-13 16:27:48 +0300

    Делает главное меню интерактивным

---
+++ source/js/navigationbutton.js
@@ -0,0 +1,10 @@
+let openNavButton = document.querySelector('.main-navigation__toggle');
+let changeNavButtonImage = document.querySelector('.main-navigation__toggle-image');
+let mainNavigationShow = document.querySelector('.main-navigation__list');
+
+openNavButton.onclick = function() {
+  openNavButton.classList.toggle('main-navigation__toggle--open');
+  changeNavButtonImage.classList.toggle('main-navigation__toggle-image--open');
+  mainNavigationShow.classList.toggle('main-navigation__list--closed');
+  console.log('Кнопка нажата');
+}
