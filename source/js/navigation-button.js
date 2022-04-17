70128cee28071066aede7f097ae66e1ed7f4046a
Regeace <playmarket_pochta@mail.ru>
2022-04-17 18:14:12 +0300

    Дорабатывает навигацию

--- source/js/navigation-button.js
+++ source/js/navigation-button.js
@@ -2,6 +2,10 @@ let openNavButton = document.querySelector('.main-navigation__toggle');
 let changeNavButtonImage = document.querySelector('.main-navigation__toggle-image');
 let mainNavigationShow = document.querySelector('.main-navigation__list');
 
+document.querySelector('.main-header').classList.remove('main-header--nojs');
+document.querySelector('.main-navigation__toggle').classList.remove('main-navigation__toggle--nojs');
+document.querySelector('.main-navigation__list').classList.remove('main-navigation__list--nojs');
+
 openNavButton.onclick = function() {
   openNavButton.classList.toggle('main-navigation__toggle--open');
   changeNavButtonImage.classList.toggle('main-navigation__toggle-image--open');
